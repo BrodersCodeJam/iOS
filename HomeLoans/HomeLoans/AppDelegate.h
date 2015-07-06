9865dbb3e11e24b88b95a87f4df305752f80e358
Mark Willis <markwillis81@gmail.com>
2015-07-06 13:59:36 +0200

    Initial commit

---
+++ HomeLoans/HomeLoans/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeLoans
+//
+//  Created by Mark on 2015/07/06.
+//  Copyright (c) 2015 MW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
